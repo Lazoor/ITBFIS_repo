084cad58e078c61ffa6579d532a1c6e474012191
DESKTOP-PLVOIEA\Lasse <lasse_siv@hotmail.com>
2016-11-28 11:17:30 +0100

    Added fonts and a some changes to start layout.

--- Anettes_sandwich/app/src/main/java/dk/itbfis/anettes_sandwich/StartActivity.java
+++ Anettes_sandwich/app/src/main/java/dk/itbfis/anettes_sandwich/StartActivity.java
@@ -1,13 +1,31 @@
 package dk.itbfis.anettes_sandwich;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button StartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        StartButton = (Button) findViewById(R.id.start_button);
+        Typeface font = Typeface.createFromAsset(getAssets(), "font/Roboto-Bold.ttf");
+        StartButton.setTypeface(font);
+
+        StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getApplicationContext(), SandwichChoiceActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
