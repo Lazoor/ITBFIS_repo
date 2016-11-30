26f377fe276fd5f730f63f4a15cdb6d5192d7977
HenrikBagger <black_volt@hotmail.com>
2016-11-30 13:45:04 +0100

    WIP.

--- Anettes_sandwich/app/src/main/java/dk/itbfis/anettes_sandwich/SandwichChoiceActivity.java
+++ Anettes_sandwich/app/src/main/java/dk/itbfis/anettes_sandwich/SandwichChoiceActivity.java
@@ -2,12 +2,24 @@ package dk.itbfis.anettes_sandwich;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
+
 
 public class SandwichChoiceActivity extends AppCompatActivity {
+
+    ListView sandwichListview;
+    List<ButtonObject> buttonObjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sandwich_choice);
+
+        sandwichListview = (ListView) findViewById(R.id.sandwich_listview);
+
+
     }
+
 }
