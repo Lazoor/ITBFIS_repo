18253ad7e590bac6402bdde1f9f61536535ee195
DESKTOP-PLVOIEA\Lasse <lasse_siv@hotmail.com>
2016-11-30 10:26:00 +0100

    Forgot to add some things...

---
+++ Anettes_sandwich/app/src/main/java/dk/itbfis/anettes_sandwich/SandwichChoiceActivity.java
@@ -0,0 +1,13 @@
+package dk.itbfis.anettes_sandwich;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SandwichChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sandwich_choice);
+    }
+}
